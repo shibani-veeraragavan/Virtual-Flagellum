52995da26f72264562bd99dbb5540ebd80d96f3b
shibani-veeraragavan <shibani.veeraragavan@gmail.com>
2025-07-19 15:03:34 +0200

    Update Example_Script.m

--- Example_Script.m
+++ Example_Script.m
@@ -7,17 +7,21 @@
 a = 0.01 % Filament radius
 ds = 0.02 % Length of segment
 Ns = 50 % No. of segments
-S = 9 % Dimensionless frequency of the active moment (also known as Swimming number)
-A = 15 % Dimensionless amplitude of the active moment 
-k = 4*pi % Dimensionless wavenumber of the active moment
+S = [0 0 9] % Dimensionless frequency of the active moment (also known as Swimming number) along {d1, d2, d3}
+A = [0 0 15] % Dimensionless amplitude of the active moment along {d1, d2, d3}
+k = [0 0 4*pi] % Dimensionless wavenumber of the active moment along {d1, d2, d3}
 mu = 1 % Fluid viscosity
 KB = 1 % Bending stiffness
 KT = 1 % Twisting stiffness
 wall = 1 % Initial height of the filament above the wall (for free space, set wall = 0)
-dt = 10 % Timestep size
 no_beat_cycles = 10 % No. of beat cycles to simulate 
 save_step = 10 % Print results every __ timesteps
 concheck_tol = 1e-6 % Solver error tolerance
+
+% Choose timestep size
+dtve = mu*(ds)^4/KB;   % Smallest viscoelastic timescale
+dtva = mu*(ds)^3*L/KB/max(A); % Smallest viscous-active timescale
+dt = min([dtve dtva]);
 
 % Run RPY simulation and store results in file "Virtual_Flagellum_Output.out"
 % To use RFT, replace main_RPY below with main_Ani.
